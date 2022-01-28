cd5ae4ff31aaa552dfd20273dde225a3b33c43af
Nole35 <lazic.sr9.2020@uns.ac.rs>
2022-01-28 09:16:20 +0100

    Login i Registracija

---
+++ SR09-2020-POP2021/Windows/LoginReg/PrijavaWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SR09_2020_POP2021.Model;
+
+namespace SR09_2020_POP2021.Windows.LoginReg
+{
+    /// <summary>
+    /// Interaction logic for PrijavaWindow.xaml
+    /// </summary>
+    public partial class PrijavaWindow : Window
+    {
+        public PrijavaWindow()
+        {
+            Utill.Instance.CitanjeEntiteta();
+            InitializeComponent();
+        }
+
+        private void prijava(object sender, RoutedEventArgs e)
+        {
+            SqlConnection sqlCon = new SqlConnection(Utill.CONNECTION_STRING);
+            try
+            {
+                sqlCon.Open();
+                string admin = "SELECT COUNT(1) FROM Administrator WHERE jmbg=@jmbg AND lozinka=@lozinka and aktivan=1";
+                SqlCommand sqlcommandAdmin = new SqlCommand(admin, sqlCon);
+                sqlcommandAdmin.CommandType = System.Data.CommandType.Text;
+                sqlcommandAdmin.Parameters.AddWithValue("@jmbg", txtUsername.Text);
+                sqlcommandAdmin.Parameters.AddWithValue("@lozinka", txtPassword.Password);
+                int countAdmin = (int)sqlcommandAdmin.ExecuteScalar();
+
+                string instruktor = "SELECT COUNT(1) FROM Instruktori WHERE jmbg=@jmbg AND lozinka=@lozinka and aktivan=1";
+                SqlCommand sqlcommandInstrukotr = new SqlCommand(instruktor, sqlCon);
+                sqlcommandInstrukotr.CommandType = System.Data.CommandType.Text;
+                sqlcommandInstrukotr.Parameters.AddWithValue("@jmbg", txtUsername.Text);
+                sqlcommandInstrukotr.Parameters.AddWithValue("@lozinka", txtPassword.Password);
+                int countInstruktor = (int)sqlcommandInstrukotr.ExecuteScalar();
+
+                string polaznik = "SELECT COUNT(1) FROM Polaznici WHERE jmbg=@jmbg AND lozinka=@lozinka and aktivan=1";
+                SqlCommand sqlcommandPolaznik = new SqlCommand(polaznik, sqlCon);
+                sqlcommandPolaznik.CommandType = System.Data.CommandType.Text;
+                sqlcommandPolaznik.Parameters.AddWithValue("@jmbg", txtUsername.Text);
+                sqlcommandPolaznik.Parameters.AddWithValue("@lozinka", txtPassword.Password);
+
+                int countPolaznik = (int)sqlcommandPolaznik.ExecuteScalar();
+
+
+                if (countAdmin > 0)
+                {
+                    AdminWindow adminProzor = new AdminWindow(txtUsername.Text);
+                    adminProzor.Show();
+                    this.Close();
+                }
+                else if (countInstruktor > 0)
+                {
+                    InstruktorWindow instProzor = new InstruktorWindow(txtUsername.Text);
+                    instProzor.Show();
+                    this.Close();
+                }
+                else if (countPolaznik > 0)
+                {
+                    PolaznikWindow polaznikProzor = new PolaznikWindow(txtUsername.Text);
+                    polaznikProzor.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Greska pri unosu");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+        }
+
+        private void close(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ SR09-2020-POP2021/Windows/LoginReg/RegistracijaPolaznikaWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SR09_2020_POP2021.Model;
+
+namespace SR09_2020_POP2021.Windows.LoginReg
+{
+    /// <summary>
+    /// Interaction logic for RegsitracijaPolaznikaWindow.xaml
+    /// </summary>
+    public partial class RegistracijaPolaznikaWindow : Window
+    {
+        public RegistracijaPolaznikaWindow()
+        {
+            InitializeComponent();
+            string adrese = "select * from Adrese";
+            SqlConnection sqlCon = new SqlConnection(Utill.CONNECTION_STRING);
+            sqlCon.Open();
+            SqlCommand sqlCommand = new SqlCommand(adrese, sqlCon);
+            SqlDataReader sdr = sqlCommand.ExecuteReader();
+            while (sdr.Read())
+            {
+                cbAdrese.Items.Add(sdr[0]);
+            }
+        }
+
+        private void registracijaDugme(object sender, RoutedEventArgs e)
+        {
+            string ime = txtIme.Text;
+            string prezime = txtPrezime.Text;
+            string jmbg = txtJMBG.Text;
+            string polString = cbPol.Text.ToString();
+            EPol pol = (EPol)Enum.Parse(typeof(EPol), polString, true);
+            int adresaID = int.Parse(cbAdrese.SelectedItem.ToString());
+            string email = txtEmail.Text;
+            string lozinka = txtLozinka.Text;
+            Random random = new Random();
+            int id = random.Next(200, 500);
+
+            Polaznik polaznik = new Polaznik(id, ime, prezime, lozinka, email, jmbg, adresaID, pol, true, 200);
+            Utill.Instance.Polaznici.Add(polaznik);
+            Utill.Instance.SacuvajEntitet(polaznik);
+            MessageBox.Show("Uspesna registracija");
+            PrijavaWindow prijava = new PrijavaWindow();
+            prijava.Show();
+            this.Close();
+        }
+
+        private void close(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            FitnesCentarAWindow f = new FitnesCentarAWindow();
+            f.Show();
+        }
+    }
+}
